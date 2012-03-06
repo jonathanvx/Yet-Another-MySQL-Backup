5373424ff3c82aa30c76b881467e087952cd8e37
Jonathan Levin <mail@jonathanlevin.co.uk>
2012-03-06 07:20:31 -0600

    minor tweaks

--- backup.sh
+++ backup.sh
@@ -6,6 +6,8 @@ DATE1=`date +%Y-%m-%d_%H_%M`
 STARTTIME1=`date +%s`
 BASEDIRPHYSICAL=/backups/db/physical/
 BACKUPDIRPHYSICAL=/backups/db/physical/$DATE1
+#Please note that moving a physical backup into the MySQL dir on the same partition is almost instant.
+#Keeping it on another partition could take some time to copy over to the MySQL dir.
 LOGGER=/backups/db/physical/dbbackup.log
 MAIL=mail@jonathanlevin.co.uk
 HOST=127.0.0.1
@@ -57,8 +59,8 @@ if [ $MYDUMPER == "Yes" ]; then
 
 
 DATE2=`date +%Y-%m-%d_%H_%M`
-BASEDIR2=/backups/db/logical/
-BACKUPDIR2=/backups/db/logical/$DATE2
+BASEDIRLOGICAL=/backups/db/logical/
+BACKUPDIRLOGICAL=/backups/db/logical/$DATE2
 LOGGER=/backups/db/logical/dbdump.log
 STARTTIME2=`date +%s`
 
@@ -67,16 +69,16 @@ if [ "$1" == "full" ]; then
         tail -f $LOGGER &
 fi
 
-mydumper -o $BACKUPDIR2 -r 500000 -e -c -l 2000 -v 3 -h 127.0.0.1 -h $HOST -u $USER -p $PASSWORD 2>> $LOGGER
+mydumper -o $BACKUPDIRLOGICAL -r 500000 -e -c -l 2000 -v 3 -h 127.0.0.1 -h $HOST -u $USER -p $PASSWORD 2>> $LOGGER
 if [[ $(egrep --quiet 'Error|CRITICAL' $LOGGER) ]]; then
         grep --quiet 'Error|CRITICAL' $LOGGER | mutt -a $LOGGER -s "TestDB MyDumper FAILED" $MAIL
-	rmdir --ignore-fail-on-non-empty $BACKUPDIR2
+	rmdir --ignore-fail-on-non-empty $BACKUPDIRLOGICAL
         rm -f $LOGGER
 else
         if [ "$1" == "full" ]; then
 	        ps -ef | grep tail | grep dbdump.log | awk '{print $2}' | xargs kill -9
         fi
-	mv $LOGGER $BACKUPDIR2
+	mv $LOGGER $BACKUPDIRLOGICAL
 
 	echo "Removing old logical backups"
         #removing old backups - 7 days old
@@ -95,16 +97,16 @@ if [ -d $BACKUPDIRPHYSICAL ]; then
         echo `expr $STARTTIME2 - $STARTTIME1` | awk '{print "Time taken:", strftime("%H:%M:%S", $1+21600)}' >> $LOGGER
         echo >> $LOGGER
 fi
-if [ -d $BACKUPDIR2 ] && [ $MYDUMPER == "Yes" ]; then
-        echo "MyDumper Saved to: " $BACKUPDIR2 >> $LOGGER
-        du -h $BACKUPDIR2 | tail -1 | awk '{ print "Backup storage size: ",$1}' >> $LOGGER
-        df -h $BACKUPDIR2 | tail -1 | awk '{ print "Available space on partition: " $4, ", Available Percent: "$5}' >> $LOGGER
+if [ -d $BACKUPDIRLOGICAL ] && [ $MYDUMPER == "Yes" ]; then
+        echo "MyDumper Saved to: " $BACKUPDIRLOGICAL >> $LOGGER
+        du -h $BACKUPDIRLOGICAL | tail -1 | awk '{ print "Backup storage size: ",$1}' >> $LOGGER
+        df -h $BACKUPDIRLOGICAL | tail -1 | awk '{ print "Available space on partition: " $4, ", Available Percent: "$5}' >> $LOGGER
 	ENDTIME=`date +%s`
         echo `expr $ENDTIME - $STARTTIME2` | awk '{print "Time taken:", strftime("%H:%M:%S", $1+21600)}' >> $LOGGER
         echo >> $LOGGER
 fi
 
-if [ -d $BACKUPDIRPHYSICAL ] || [ -d $BACKUPDIR2 ] && [ $MYDUMPER == "Yes" ]; then
+if [ -d $BACKUPDIRPHYSICAL ] || [ -d $BACKUPDIRLOGICAL ] && [ $MYDUMPER == "Yes" ]; then
 	pt-slave-find --host $HOST --user $USER --password $PASSWORD | grep status >> $LOGGER
 	echo >> $LOGGER
 	ENDTIME=`date +%s`
